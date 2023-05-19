6859fb42b050f90f7d6daa632b50fcf828990c11
nao008 <naoya-0829@i.softbank.jp>
2023-05-19 15:28:50 +0900

    [UPGRADE] dir structure

---
+++ make_score.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from preprocess.preprocess import DataCleansing
+import os
+
+
+def mein():
+     current_dir = os.getcwd()
+     test_df = pd.read_csv(f"{current_dir}/data/test.csv",index_col=0)
+
+     pp = DataCleansing()
+     
+     
+
+     
+
+     X_train, X_test, y_train, y_test = pp.train_split()
+
+     # モデルの構築と学習
+     model = LinearRegression()
+     model.fit(X_train, y_train)
+     # =============================================================================
+     # # モデルの評価
+     # y_pred = model.predict(X_test)
+     # y_pred = pd.DataFrame(y_pred)
+     # y_pred = y_pred.round().astype(int)
+     # =============================================================================
+
+     y_pred_test = model.predict(test_df)
+     y_pred_test = pd.DataFrame(y_pred_test,index=test_df.index)
+     y_pred_test = y_pred_test.round().astype(int)
+
+
+     # y_pred_test.to_csv(f"submits/{datetime.datetime.today().strftime('%Y%m%d')}submit.csv",header=False)
+
+
+
+
+
+
+
+
+if __name__ == "__mein__":
+     mein()
---
+++ preprocessor/preprocess.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import os
+
+directory = os.getcwd()
+# directory = os.path.dirname(directory)
+
+class DataCleansing():
+     def __init__(self,original_df=pd.read_csv(f"{directory}/data/train.csv",index_col=0)):#trainデータを取得
+          self.original_x = original_df.drop(['price_range'],axis=1)
+          self.original_y = original_df["price_range"]
+          
+     def rt_train_data(self):
+          x = self.original_x
+          y = self.original_y
+          return x, y
+     
+     def train_split(self):
+          #学習データと検証用データに分割
+          X_train, X_test, y_train, y_test = train_test_split(self.original_x, self.original_y,
+                                                                   test_size=0.3,
+                                                                   random_state=42
+                                                                   )
+          return X_train, X_test, y_train, y_test
+          
